c21903fab5571cab2ed04a112c1f198a04da0989
Lee Chilvers <lee.chilvers@icloud.com>
2021-03-08 12:15:04 +0000

    • Created General • Added logic to DataCollectorViewModel which gets unix time from timers • Ready to save the timestamps in database

---
+++ app/src/main/java/com/example/spmapp/Helpers/General.java
@@ -0,0 +1,8 @@
+package com.example.spmapp.Helpers;
+
+public class General {
+
+    public static long getUnixTime() {
+        return System.currentTimeMillis() / 1000L;
+    }
+}
--- app/src/main/java/com/example/spmapp/ViewModels/DataCollectorViewModel.java
+++ app/src/main/java/com/example/spmapp/ViewModels/DataCollectorViewModel.java
@@ -1,6 +1,54 @@
 package com.example.spmapp.ViewModels;
 
-public class DataCollectorViewModel {
+import android.app.Application;
+import android.content.Context;
+import android.content.SharedPreferences;
 
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
 
+import com.example.spmapp.Helpers.General;
+
+public class DataCollectorViewModel extends AndroidViewModel {
+
+    SharedPreferences timerPrefs;
+
+//    SleepRepository
+//    ScreenTimeRepository
+
+    public DataCollectorViewModel(@NonNull Application application, Context context) {
+        super(application);
+        this.timerPrefs = context.getSharedPreferences("timer_prefs", Context.MODE_PRIVATE);
+    }
+
+    public void recordSleepPeriod(Long startTime, Long endTime) {
+//        sleepRepository.insert(startTime, endTime)
+    }
+
+    public void recordScreenPeriod(Long startTime, Long endTime) {
+//        screenTimeRepository.insert(startTime, endTime)
+    }
+
+    public void startTimer() {
+        SharedPreferences.Editor prefsEditor = timerPrefs.edit();
+        prefsEditor.putLong("timer_start_time", General.getUnixTime());
+        prefsEditor.apply();
+    }
+
+    public void endTimer(Boolean isSleepMode) {
+        Long startTime = getTimerStart();
+        if (startTime != 0) {
+            Long endTime = General.getUnixTime();
+
+            if (isSleepMode) {
+                recordSleepPeriod(startTime, endTime);
+            } else {
+                recordScreenPeriod(startTime, endTime);
+            }
+        }
+    }
+
+    public Long getTimerStart() {
+        return timerPrefs.getLong("timer_start_time", 0);
+    }
 }
