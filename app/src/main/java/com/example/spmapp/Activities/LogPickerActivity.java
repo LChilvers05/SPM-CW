e6d0b0403d394409b0a94ff8cfc3990da40803f5
ab3308BATH <75852160+ab3308BATH@users.noreply.github.com>
2021-03-10 16:04:47 +0000

    Renamed intents in LogPickerActivity.java

--- app/src/main/java/com/example/spmapp/Activities/LogPickerActivity.java
+++ app/src/main/java/com/example/spmapp/Activities/LogPickerActivity.java
@@ -19,12 +19,12 @@ public class LogPickerActivity extends AppCompatActivity {
     }
 
     public void logSleepTapped(View view) {
-        Intent dataCollection = new Intent(this, SleepCollectorActivity.class);
-        startActivity(dataCollection);
+        Intent sleepCollection = new Intent(this, SleepCollectorActivity.class);
+        startActivity(sleepCollection);
     }
 
     public void logScreenTimeTapped(View view) {
-        Intent dataCollection = new Intent(this, ScreenCollectorActivity.class);
-        startActivity(dataCollection);
+        Intent screenCollection = new Intent(this, ScreenCollectorActivity.class);
+        startActivity(screenCollection);
     }
 }
