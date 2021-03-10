b489d814f6a830b5e8038f4e4dcb2be9a06ae006
ab3308BATH <75852160+ab3308BATH@users.noreply.github.com>
2021-03-10 14:15:21 +0000

    - Added UI for data collection (activity_data_collection.xml). - Added class to manage this UI (DataCollectorActivity) - Updated strings.xml.

---
+++ app/src/main/java/com/example/spmapp/Activities/DataCollectorActivity.java
@@ -0,0 +1,17 @@
+package com.example.spmapp.Activities;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.spmapp.R;
+import com.example.spmapp.ViewModels.DataCollectorViewModel;
+
+public class DataCollectorActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_data_collection);
+    }
+
+}
