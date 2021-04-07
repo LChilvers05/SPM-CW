0cb83285817ae720d0fb47e202c1f3e5b6e2e076
ab3308BATH <75852160+ab3308BATH@users.noreply.github.com>
2021-04-07 16:51:28 +0100

    Added privacy agreement page. Not connected to rest of app yet. Accept policy button not yet functional.

---
+++ app/src/main/java/com/example/spmapp/Activities/UserPreferences.java
@@ -0,0 +1,16 @@
+package com.example.spmapp.Activities;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.spmapp.R;
+
+public class UserPreferences extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_preferences);
+    }
+}
