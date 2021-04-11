831645ba483a42d5ef9357e133ec029e0c341a3f
ab3308BATH <75852160+ab3308BATH@users.noreply.github.com>
2021-04-11 19:15:01 +0100

    Changed privacy agreement to use CheckBox instead of button. Added SharedPreferences to track agreement of privacy policy.

--- app/src/main/java/com/example/spmapp/Activities/PrivacyAgreement.java
+++ app/src/main/java/com/example/spmapp/Activities/PrivacyAgreement.java
@@ -1,16 +1,33 @@
 package com.example.spmapp.Activities;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.view.View;
+import android.widget.CheckBox;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.spmapp.R;
 
 public class PrivacyAgreement extends AppCompatActivity {
+
+    CheckBox privacyCheckbox;
+    SharedPreferences sharedPreferences;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_privacy_agreement);
 
+        privacyCheckbox = findViewById(R.id.privacyAgreementCheckbox);
+        sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+
+        boolean policyAgreed = sharedPreferences.getBoolean("policyAgreed", false);
+        privacyCheckbox.setChecked(policyAgreed);
+    }
+
+    public void checkboxClick(View view){
+        sharedPreferences.edit().putBoolean("policyAgreed", ((CheckBox) view).isChecked()).commit();
     }
 }
