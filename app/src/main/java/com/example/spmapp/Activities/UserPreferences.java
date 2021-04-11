145453e8f2cf66f631b2558526863673a25e1456
ab3308BATH <75852160+ab3308BATH@users.noreply.github.com>
2021-04-11 19:07:17 +0100

    Added SharedPreferences to user data collection preference page.

--- app/src/main/java/com/example/spmapp/Activities/UserPreferences.java
+++ app/src/main/java/com/example/spmapp/Activities/UserPreferences.java
@@ -1,6 +1,10 @@
 package com.example.spmapp.Activities;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.view.View;
+import android.widget.CheckBox;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -8,9 +12,37 @@ import com.example.spmapp.R;
 
 public class UserPreferences extends AppCompatActivity {
 
+    CheckBox screenCheck;
+    CheckBox sleepCheck;
+    SharedPreferences sharedPreferences;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_preferences);
+
+        screenCheck = findViewById(R.id.screentimeCheckBox);
+        sleepCheck = findViewById(R.id.sleepCheckBox);
+
+        sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+
+        boolean screenChecked = sharedPreferences.getBoolean("screenCheck", false);
+        boolean sleepChecked = sharedPreferences.getBoolean("sleepCheck", false);
+
+        screenCheck.setChecked(screenChecked);
+        sleepCheck.setChecked(sleepChecked);
+
     }
+
+    public void checkboxClick(View view){
+        switch(view.getId()){
+            case R.id.sleepCheckBox:
+                sharedPreferences.edit().putBoolean("sleepCheck", ((CheckBox) view).isChecked()).commit();
+                break;
+            case R.id.screentimeCheckBox:
+                sharedPreferences.edit().putBoolean("screenCheck", ((CheckBox) view).isChecked()).commit();
+                break;
+        }
+    }
+
 }
